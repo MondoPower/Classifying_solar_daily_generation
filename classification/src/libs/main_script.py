efa1e8760adfbdbe24a9faaf135fa882f8be93f7
zara <zara.zarei@ausnetservices.com.au>
2023-01-17 15:35:14 +1100

    new changes to path of folders

---
+++ classification/src/libs/main_script.py
@@ -0,0 +1,31 @@
+from more_itertools import one
+import data_gathering
+import pandas as pd
+import etl.influx_etl as influx_etl
+import os
+import numpy as np
+import pandas as pd
+import pdb
+
+
+
+location_id = '0780ec0b-56fa-4242-88ae-0355069045bf'
+daterange = {"start_date": '2022-08-19T00:00:00Z', "end_date": '2022-10-19T00:00:00Z'}
+start_date,end_date = data_gathering.get_date_range(daterange)
+loc_data, location_config = data_gathering.get_location_full(location_id)
+
+# for loc in locations.location_id.unique()[0:1]:
+#     final_df, pv_training_cols, weather_cols = data_gathering.gather_dataset_for_location(loc)
+#     training_data, target = data_wrangling.add_timeseries_cols_and_format(final_df, 'pv')
+#     output_models, train_df, test_df, train_time, test_time, pred_test, pred_train  = model_training.main_model_generation(training_data, loc)
+#     #model_training.plot_models(test_df, train_df, test_time, train_time, pred_test, pred_train, loc)
+#     #model_training.plot_error_models(test_df, train_df, test_time, train_time, pred_test, pred_train, loc)
+#     output_df = pd.DataFrame(output_models)
+#     metrics = metrics.append(output_df)
+#     #print(calc_metrics)
+#     filepath = f'{loc} xgb_test.csv'
+#     if os.path.exists(filepath) == True:
+#          metrics.to_csv(filepath, mode='a', header=False)
+#     else:
+#          metrics.to_csv(filepath)
+
--- src/lambdas/classification/src/test_handler.py
+++
@@ -1,9 +0,0 @@
-from lambda_function import handler
-#Aviva Communities
-
-event = {"community_name": "Aviva Communities","report_type": "quarterly"}
-
-result = handler(event, 'UnimportantTrash')
-
-# print('-------RESULT-------')
-# print(result)
